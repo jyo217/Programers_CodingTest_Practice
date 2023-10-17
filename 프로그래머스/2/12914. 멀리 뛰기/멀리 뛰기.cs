47919d4949fde5b88912c64e6ef6bccbcdec5f79
jyo217 <141624608+jyo217@users.noreply.github.com>
2023-10-17 09:23:21 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.19 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    long[] howMany = new long[2000];
+    
+    public long solution(int n) {
+        howMany[0] = 1;
+        howMany[1] = 2;
+        
+        for (int i = 2; i < n; i++)
+            howMany[i] = (howMany[i - 2] + howMany[i - 1]) % 1234567;
+        
+        return howMany[n - 1];
+    }
+}
